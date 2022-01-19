cb17344d4decbb5ca27c6e212ec7be6fa17e5e0c
swaraj <swaraj.trivedi2018@vitstudent.ac.in>
2022-01-19 21:50:48 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var latitude; //to get the latitude
+var longitude; //to get the longitude
+var latlongvalue; //to specify the coordinates in img
+var x = document.getElementById('location');// to fetch the element with id="location"
+function getlocation() //to fetch location
+{
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		document.getElementById('location').innerHTML = 'Browser Does not Support';
+	}
+}
+var latitude; //to get the latitude
+var longitude; //to get the longitude
+var latlongvalue; //to specify the coordinates in img
+var x; // to fetch the element with id="location"
+function showPosition(position) {
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+	latlongvalue = position.coords.latitude + ',' + position.coords.longitude;
+	img_url =
+		'https://maps.googleapis.com/maps/api/staticmap?center=' +
+		latlongvalue +
+		'&zoom=17&size=500x300&key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o';
+	document.getElementById('mapholder').innerHTML = "<img src='" + img_url + "'>";
+	x = 'Latitude: ' + position.coords.latitude + ', ' + '<br>Longitude: ' + position.coords.longitude;
+	document.getElementById('location').innerHTML = x;
+}
